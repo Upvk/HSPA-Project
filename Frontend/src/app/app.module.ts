a5e0b55943ec59a92ea7e4b7861e5e5da849a353
Vivek Upadhyay <Vivek.Upadhyay@odlindia.com>
2024-12-30 20:41:19 +0530

    Added Json File and http get request

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PropertyModule } from './property/property.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+//import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     NavBarComponent
    ],
   imports: [BrowserModule , PropertyModule],
-  providers: [],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
@@ -6,39 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './property-list.component.html',
   styleUrl: './property-list.component.css'
 })
-export class PropertyListComponent {
-  
-  properties: Array<any> = 
-  [
-    {
-      "Id":1,
-      "Name": "Birla House",
-      "Type":"House",
-      "Price":12000
-    },
-    {
-      "Id":2,
-      "Name": "Cipla House",
-      "Type":"Farm House",
-      "Price":17000
-    },
-    {
-      "Id":3,
-      "Name": "Sharma House",
-      "Type":"Flat",
-      "Price":10000
-    },
-    {
-      "Id":4,
-      "Name": "Mishra House",
-      "Type":"Building",
-      "Price":50000
-    },
-    {
-      "Id":5,
-      "Name": "Velco Villa",
-      "Type":"Villa",
-      "Price":30000
-    }
-  ] 
+export class PropertyListComponent implements OnInit {
+
+  properties: any;
+
+  constructor(private http:HttpClient){};
+
+  ngOnInit(): void
+  {
+    this.http.get('data/properties.Json').subscribe
+    (
+      data=>
+      {
+        this.properties = data;
+        console.log(data);
+      }
+    )
+  }
 }
