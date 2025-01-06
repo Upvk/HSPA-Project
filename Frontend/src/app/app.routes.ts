18467db1578c05d59fe12f6eb4d5e8d0f7077220
Vivek <officialupadhyayvivek@gmail.com>
2025-01-06 06:53:22 +0530

    added register and login

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
-export const routes : Routes = [
-    {path: 'property-list', component: PropertyListComponent},
-    {path: 'add-property', component: AddPropertyComponent},
-    {path: 'property-detail/:id', component:PropertyDetailComponent},
+export const routes : Routes = 
+[
+    {path: 'property/list', component: PropertyListComponent},
+    {path: 'property/add', component: AddPropertyComponent},
+    {path: 'property/detail/:id', component:PropertyDetailComponent},
+    {path: 'user/login', component:UserLoginComponent},
+    {path: 'user/register', component:UserRegisterComponent},
+    //this '**' path should always be last!
     {path: '**', component:PropertyListComponent}
-  ];
+];
