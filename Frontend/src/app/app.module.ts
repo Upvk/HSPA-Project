353da4dfea615aeca7926039d06df1f3f5bb25c1
Vivek <officialupadhyayvivek@gmail.com>
2025-01-08 21:28:04 +0530

    adding custom validator but its failing

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,19 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PropertyModule } from './property/property.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-//import { HttpClientModule } from '@angular/common/http'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HosuingService } from './services/hosuing.service';
 import { RouterModule } from '@angular/router';
 import { routes } from 'app/app.routes';
 import { FormsModule } from '@angular/forms';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 
   declarations: 
   [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    UserLoginComponent,
+    UserRegisterComponent
   ],
 
   imports: 
@@ -23,7 +27,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule ,
     PropertyModule,
     RouterModule.forRoot(routes),
-    FormsModule 
+    FormsModule ,
+    ReactiveFormsModule
   ],
 
   providers: 
--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { matchingpassword } from '../../util/validators/validators-function.validator';
+
 
 @Component({
   selector: 'app-user-register',
-  standalone:false,
+  standalone: false,
   templateUrl: './user-register.component.html',
   styleUrl: './user-register.component.css'
 })
-export class UserRegisterComponent {
+export class UserRegisterComponent implements OnInit {
 
+  registerationForm!: FormGroup;
+
+  constructor(){};
+
+  ngOnInit(): void {
+    this.registerationForm = new FormGroup(
+      {
+        name : new FormControl('', [Validators.required, Validators.minLength(5)]),
+        email : new FormControl('', [Validators.required, Validators.email]),
+        password : new FormControl('', [Validators.required, Validators.minLength(5)]),
+        confirmPassword : new FormControl('', [Validators.required, Validators.minLength(5)]),
+        mobileNumber : new FormControl('', [Validators.minLength(10)])
+      }, 
+      { validators: matchingpassword } // Apply the custom validator to the FormGroup
+    );
+  }
+
+  OnSubmit()
+  {
+    console.log(this.registerationForm);
+  }
 }
